895f595ddbb8aefa19121556490d266e6c676cd2
shammitra <sham.mitra@gmail.com>
2021-08-01 22:15:14 +0100

    COVID-19 vaccinations - countries only using the Oxford/AstraZeneca vaccine.

--- COVID-19_vaccinations.sql
+++ COVID-19_vaccinations.sql
@@ -6,3 +6,5 @@ SELECT cv1.country, cv1.vaccines, SUM(cv1.total_vaccinations)
 FROM country_vaccinations cv1, country_vaccinations cv2 
 WHERE cv1.country = cv2.country AND cv1.vaccines = "Oxford/AstraZeneca" 
 GROUP BY cv1.country;
+
+# The countries are: Antigua and Barbuda, Barbados, and Botswana.
