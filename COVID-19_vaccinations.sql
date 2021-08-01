161f5e4c4c82422c78c9420cbff0b3db401a8b1c
shammitra <sham.mitra@gmail.com>
2021-08-01 22:11:35 +0100

    Querying Oxford/AstraZeneca vaccination use.

---
+++ COVID-19_vaccinations.sql
@@ -0,0 +1,8 @@
+# Querying which countries solely use the Oxford/AstraZeneca vaccine.
+# This query uses a self join on the country_vaccinations table.
+# Data available at: https://www.kaggle.com/gpreda/covid-world-vaccination-progress.
+
+SELECT cv1.country, cv1.vaccines, SUM(cv1.total_vaccinations)
+FROM country_vaccinations cv1, country_vaccinations cv2 
+WHERE cv1.country = cv2.country AND cv1.vaccines = "Oxford/AstraZeneca" 
+GROUP BY cv1.country;
